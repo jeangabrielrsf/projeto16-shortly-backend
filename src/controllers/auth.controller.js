3d035aecd27e058f486d1a2e87a1e9328f880796
Jean Frusca <jeang96@gmail.com>
2022-10-14 21:58:05 -0300

    feat: users can now sign in.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,11 +51,11 @@ async function signUp(req, res) {
 
 async function signIn(req, res) {
 	try {
-		const { email, passsword } = req.body;
+		const { email, password } = req.body;
 		const validation = signInSchema.validate(
 			{
 				email,
-				passsword,
+				password,
 			},
 			{ abortEarly: false }
 		);
@@ -64,12 +64,39 @@ async function signIn(req, res) {
 				.status(422)
 				.send(validation.error.details.map((item) => item.message));
 		}
+		const userExists = await connection.query(
+			`
+            SELECT * FROM users WHERE email = $1;
+        `,
+			[email]
+		);
+		if (userExists.rowCount == 0) {
+			return res.sendStatus(401);
+		}
 
-		return res.sendStatus(200);
+		const decryptPassword = bcrypt.compareSync(
+			password,
+			userExists.rows[0].password
+		);
+		console.log(decryptPassword);
+		if (!decryptPassword) {
+			return res.sendStatus(401);
+		}
+
+		console.log(userExists.rows[0].id);
+		const token = uuid();
+		connection.query(
+			`
+		    INSERT INTO sessions (token, "userId") VALUES ($1, $2);
+		`,
+			[token, userExists.rows[0].id]
+		);
+
+		return res.status(200).send(token);
 	} catch (error) {
 		console.log(error);
 		return res.sendStatus(500);
 	}
 }
 
-export { signUp };
+export { signUp, signIn };
