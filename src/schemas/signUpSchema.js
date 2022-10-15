f63949e596f55a49003fe81934a888009c6ebc20
Jean Frusca <jeang96@gmail.com>
2022-10-14 22:10:09 -0300

    fix(signUp): server was crashing due to issues iwht confirmPassword

--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -4,7 +4,7 @@ const signUpSchema = Joi.object({
 	name: Joi.string().empty().required(),
 	email: Joi.string().empty().email({ minDomainSegments: 2 }).required(),
 	password: Joi.string().empty().required(),
-	confirmPassword: Joi.ref("password").required(),
+	confirmPassword: Joi.ref("password"),
 });
 
 export default signUpSchema;
